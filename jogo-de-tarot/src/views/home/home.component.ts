829c31b02acc88c290bc2f6c890057f19139e448
Leonardo Oliveira <71794931+leotilt@users.noreply.github.com>
2022-12-26 17:53:31 -0300

    concat image with array

--- jogo-de-tarot/src/views/home/home.component.ts
+++ jogo-de-tarot/src/views/home/home.component.ts
@@ -15,15 +15,18 @@ export class HomeComponent implements OnInit {
   cards!: Array<any>;
   imagesUrl!: string;
   imageBackCard!: string;
-  name?:any
+  name?: any;
+  image?: any;
 
-
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    
+  }
 
   updateData() {
     this.dataCards.getDataCards().subscribe((data) => {
       this.data = data;
       this.pointData();
+      this.concatURL()
     });
   }
 
@@ -31,6 +34,11 @@ export class HomeComponent implements OnInit {
     this.imageBackCard = this.data.imageBackCard;
     this.imagesUrl = this.data.imagesUrl;
     this.cards = this.data.cards;
-    this.name = this.cards[0].name
+    // this.name = this.cards[0].name;
   }
+ 
+ concatURL(){
+ this.image = this.imagesUrl.concat(this.cards[0].image)
+ } 
+
 }
