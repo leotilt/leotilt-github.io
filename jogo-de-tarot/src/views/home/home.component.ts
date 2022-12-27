4d35ebb1e8498d322bff851bdb42ed5521bdf7bf
Leonardo Oliveira <71794931+leotilt@users.noreply.github.com>
2022-12-27 17:40:05 -0300

    alterações para exibir cartas na tela

--- jogo-de-tarot/src/views/home/home.component.ts
+++ jogo-de-tarot/src/views/home/home.component.ts
@@ -1,3 +1,5 @@
+import { TOUCH_BUFFER_MS } from '@angular/cdk/a11y/input-modality/input-modality-detector';
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { CardsAPIService } from 'src/components/services/cards-api.service';
 
@@ -15,38 +17,47 @@ export class HomeComponent implements OnInit {
   cards!: Array<any>;
   imagesUrl!: string;
   imageBackCard!: string;
-  name?: any;
-  image?: any;
+  name?: string;
+  image?: string;
+
+  namesf!: any;
+  imagesf?: string[] = [];
 
   shufflerValue!: any;
 
-  ngOnInit(): void {
-    
-  }
+  ngOnInit(): void {}
 
   updateData() {
     this.dataCards.getDataCards().subscribe((data) => {
       this.data = data;
-      this.pointData();
-      this.shuffleArray();
+      this.cards = this.data.cards;
+      this.imageBackCard = this.data.imageBackCard;
+      this.imagesUrl = this.data.imagesUrl;
+
+      this.shufflerCardsArray();
+      this.startTarotCards();
+      this.startTarotCards()
     });
   }
 
-  pointData() {
-    this.imageBackCard = this.data.imageBackCard;
-    this.imagesUrl = this.data.imagesUrl;
-    this.cards = this.data.cards;
-  }
+  startTarotCards() {
+     let suffix = this.shufflerValue.image
+     for(const suffixs of suffix){
+      const newUrl =`${this.imagesUrl}${suffixs}`;
 
-  concatURL() {
+     console.log(newUrl)
+     }
+    }
+ 
+
+  concatShufflerValue() {
     this.image = this.imagesUrl.concat(this.shufflerValue.image);
     this.name = this.shufflerValue.name;
-    
   }
 
-  shuffleArray() {
-    let shuffler = Math.floor(Math.random() * this.cards.length);
-    this.shufflerValue = this.cards[shuffler];
-    this.concatURL()
+  shufflerCardsArray() {
+    let random = Math.floor(Math.random() * this.cards.length);
+    this.shufflerValue = this.cards[random];
+    this.concatShufflerValue();
   }
 }
